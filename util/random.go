551e49df788dd93e8c2acd2afb4cc5b6e86a855d
chenweifan <chensheep1005@gmail.com>
2023-04-23 16:47:46 +0800

    test: 💍 Added CRUD operation test for some tables

---
+++ util/random.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		sb.WriteByte(alphabet[rand.Intn(k)])
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(8)
+}
+
+func RandomBalance() int64 {
+	return RandomInt(0, 2000)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 2000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "BTC"}
+	return currencies[rand.Intn(len(currencies))]
+}
