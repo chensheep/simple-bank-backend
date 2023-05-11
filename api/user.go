0781ef3c70cf821ae488bc6da44e2252d42546e5
chenweifan <chensheep1005@gmail.com>
2023-05-11 19:35:21 +0800

    feat: 🎸 Implement endpoint of creating user

---
+++ api/user.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	db "github.com/chensheep/simple-bank-backend/db/sqlc"
+	"github.com/chensheep/simple-bank-backend/util"
+	"github.com/lib/pq"
+
+	"github.com/gin-gonic/gin"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum,min=4,max=255"`
+	Password string `json:"password" binding:"required,min=6,max=255"`
+	FullName string `json:"full_name" binding:"required,min=4,max=255"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, createUserResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	})
+}
